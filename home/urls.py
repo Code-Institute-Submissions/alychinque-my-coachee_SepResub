e05c6d07d3b208d4f7c1487b28716ca17bda984d
alychinque <alychinque@gmail.com>
2021-06-21 10:48:39 +0100

    Routes and functions were created to swap pages.

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about.html', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('plans', views.plans, name='plans'),
+    path('pillars', views.pillars, name='pillars'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
-    
     return render(request, 'home/index.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+
+        #send an email
+        send_mail(
+            'New message from ' + name, #subject
+            message, #message
+            email, #from email
+            ['alychinque@gmail.com'], #to email
+        )
+        return render(request, 'home/contact.html', {'name': name})
+    else:
+        return render(request, 'home/contact.html', {})
+
+def about(request):
+    return render(request, 'home/about.html')
+
+def pillars(request):
+    return render(request, 'home/pillars.html')
+
+def plans(request):
+    return render(request, 'home/plans.html')
