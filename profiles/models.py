8af96e97bf0e77ae5c238e4f7cdb6451a28678aa
alychinque <alychinque@gmail.com>
2021-06-23 02:29:57 +0100

    Created Coach and Coachee classes in models.py.

--- profiles/models.py
+++ profiles/models.py
@@ -2,13 +2,38 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Coach(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Full Name')
-    birthday = models.CharField(max_length=11, verbose_name='Birthday')
-    phone = models.CharField(max_length=15)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, verbose_name='Full Name')
+    date_of_birth = models.DateField(verbose_name='Date Of Birth')
+    phone_number = models.CharField(max_length=20)
     list_gender = (
         ("male", "M"),
         ("female", "F"),
         ("Other", "Other"),
     )
     gender = models.CharField(max_length=8, choices=list_gender, verbose_name='Gender')
+
+    class Meta:
+        verbose_name = 'Coach'
+        verbose_name_plural = 'Coaches'
+
+    def  __str__(self):
+	    return self.name
+
+class Coachee(models.Model):
+    coach = models.ForeignKey('Coach', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=50, verbose_name='Full Name')
+    email = models.EmailField(max_length=254)
+    date_of_birth = models.DateField(verbose_name='Date Of Birth')
+    phone_number = models.CharField(max_length=20)
+    list_gender = (
+        ("male", "M"),
+        ("female", "F"),
+        ("Other", "Other"),
+    )
+    gender = models.CharField(max_length=8, choices=list_gender, verbose_name='Gender')
+    list_indications = models.TextField()
+
+    def  __str__(self):
+	    return self.name
+
