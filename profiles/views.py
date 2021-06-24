abeed2c8977b5bb831a00e5f943d28722fcd8c91
alychinque <alychinque@gmail.com>
2021-06-24 09:58:05 +0100

    Create two views, create coach and list coach.

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import View, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import *
+#from .forms import *
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+
+class CoachCreate(CreateView):
+    model = Coach
+    fields = [
+        'name', 'date_of_birth', 'phone_number', 'gender'
+    ]
+    success_url = "/"
+    template_name = "profiles/coach_create.html"
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+        instance.user = self.request.user
+        instance.save()
+        return super(CoachCreate, self).form_valid(form)
+
+class CoachList(ListView):
+	model = Coach
+	template_name = "profiles/coach_list.html"
+	context_object_name = 'coaches'
+	paginate_by = 10
+	queryset = Coach.objects.all()
+
+#view delete
+#view update
