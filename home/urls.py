fe2755f159139cd4816a243592aad0d4c0753ed7
alychinque <alychinque@gmail.com>
2021-06-24 10:06:06 +0100

    fix the urls and added coach_page.

--- home/urls.py
+++ home/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='home'),
-    path('about.html', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
-    path('plans', views.plans, name='plans'),
-    path('pillars', views.pillars, name='pillars'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('plans/', views.plans, name='plans'),
+    path('pillars/', views.pillars, name='pillars'),
+    path('coach_page/', views.coach_page, name='coach_page'),
 ]
