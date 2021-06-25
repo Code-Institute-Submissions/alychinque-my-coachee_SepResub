ac21447e012e2a7541f2c7f285b7c01d61319a04
alychinque <alychinque@gmail.com>
2021-06-25 15:16:19 +0100

    Fixed the urls and created CRUD of coachee.

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import *
 
+
+app_name = 'profiles'
+
 urlpatterns = [
-    path('coach_create', CoachCreate.as_view(), name='coach_create'),
-    path('coach_list', CoachList.as_view(), name='coach_list'),
+    path('coach_create/', CoachCreate.as_view(), name='coach_create'),
+    path('coach_list/', CoachList.as_view(), name='coach_list'),
+    path('coachee_create/', CoacheeCreate.as_view(), name='coachee_create'),
+    path('coachee_list/', CoacheeList.as_view(), name='coachee_list'),
+    path('coachee_edit/<int:pk>/', CoacheeEdit.as_view(), name='coachee_edit'),
+    path('coachee_delete/<int:pk>/', CoacheeDelete.as_view(), name='coachee_delete'),
 ]
