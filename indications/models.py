33ec1463cda108a993825ac39587d58982e2b596
alychinque <alychinque@gmail.com>
2021-07-02 11:49:30 +0100

    Fixed indications category.

--- indications/models.py
+++ indications/models.py
@@ -4,13 +4,14 @@ class Indication(models.Model):
     title = models.CharField(max_length=80)
     description = models.TextField()
     list_category = (
-        ('1', 'Movie')
-        ('2', 'Video')
-        ('3', 'Book')
-        ('4', 'Podcast')
-        ('5', 'Activity')
+        ('Movie', 'Movie'),
+        ('Video', 'Video'),
+        ('Book', 'Book'),
+        ('Podcast', 'Podcast'),
+        ('Activity', 'Activity'),
     )
     category = models.CharField(max_length=30, choices=list_category, verbose_name='Category') 
 			
     def __str__(self):
     	return self.title
+
