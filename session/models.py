52ee600f927ee1a84978fb2e63c3e3a24580c23d
alychinque <alychinque@gmail.com>
2021-07-02 12:16:27 +0100

    Fixed models session.

--- session/models.py
+++ session/models.py
@@ -6,19 +6,22 @@ class Session(models.Model):
     coach = models.ForeignKey('Coach', null=True, blank=True, on_delete=models.SET_NULL)
     coachee = models.ForeignKey('Coachee', null=True, blank=True, on_delete=models.SET_NULL)
     session = models.IntegerField()
-    date_time = models.DateTimeField()
+    date = models.DateField()
+    time = models.TimeField()
     jornal = models.TextField()
     indications1 = models.ForeignKey('Indications', null=True, blank=True, on_delete=models.SET_NULL)
     indications2 = models.ForeignKey('Indications', null=True, blank=True, on_delete=models.SET_NULL)
     indications3 = models.ForeignKey('Indications', null=True, blank=True, on_delete=models.SET_NULL)
+    concluded = models.BooleanField()
 
     def __str__(self):
-    	return self.session
+    	return ('Session with ' + self.coachee.name + ' on ' + self.date + ' at ' + self.time)
 
 class Report_Session(models.Model):
     coach = models.ForeignKey('Coach', null=True, blank=True, on_delete=models.SET_NULL)
+    session = models.ForeignKey('Session', null=True, blank=True, on_delete=models.SET_NULL)
     hours_session = models.TimeField
     appointments = models.IntegerField()
 
     def __str__(self):
-    	return self.hours_session + ' ' + self.appointments
+    	return (self.coach.name + ' spent ' + self.hours_session + ' hours in sessions and has ' + self.appointments + ' appointmemts')
