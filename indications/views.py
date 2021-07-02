a1d1e280b0e7de964b6bb9fd22adf81f823a4397
alychinque <alychinque@gmail.com>
2021-07-02 11:51:35 +0100

    Created generic views for create and list indications.

--- indications/views.py
+++ indications/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.views.generic import View, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import *
+#from .forms import *
 
-# Create your views here.
+class IndicationCreate(CreateView):
+    model = Indication
+    fields = ['title', 'description', 'category']
+    success_url = "/"
+    template_name = "indications/indication_create.html"
+
+class IndicationList(ListView):
+	model = Indication
+	template_name = "indications/indication_list.html"
+	context_object_name = 'indications'
+	paginate_by = 10
+	queryset = Indication.objects.all()
