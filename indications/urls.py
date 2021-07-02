79ffb7e1175a9f30345613d47812c116545041e6
alychinque <alychinque@gmail.com>
2021-07-02 11:50:54 +0100

    created urls for indications.

---
+++ indications/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+
+app_name = 'indications'
+
+urlpatterns = [
+    path('create/', IndicationCreate.as_view(), name='create'),
+    path('list/',IndicationList.as_view(), name='list'),
+    # path('indicatione_edit/<int:pk>/', IndicationEdit.as_view(), name='indication_edit'),
+    # path('indication_delete/<int:pk>/', IndicationDelete.as_view(), name='indication_delete'),
+]
