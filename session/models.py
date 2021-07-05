bca446204040477792b69a8c8a0bc0a517d551f8
alychinque <alychinque@gmail.com>
2021-07-05 03:03:31 +0100

    Split session between session and appointmentsession.

--- session/models.py
+++ session/models.py
@@ -2,25 +2,25 @@ from profiles.models import Coachee, Coach
 from indications.models import Indication
 from django.db import models
 
-class Session(models.Model):
-    coach = models.ForeignKey(Coach, null=True, blank=True, on_delete=models.SET_NULL)
-    coachee = models.ForeignKey(Coachee, null=True, blank=True, on_delete=models.SET_NULL)
+class AppointmentSession(models.Model):
+    coach = models.ForeignKey(Coach, null=True, blank=True, on_delete=models.CASCADE)
+    coachee = models.ForeignKey(Coachee, null=True, blank=True, on_delete=models.CASCADE)
     session = models.IntegerField()
     date = models.DateField()
     time = models.TimeField()
+
+    def __str__(self):
+    	return f'Coach {self.coach.name} session with {self.coachee.name} on {self.date} at {self.time}.'
+
+class Session(models.Model):
+    coach = models.ForeignKey(Coach, null=True, blank=True, on_delete=models.CASCADE)
+    coachee = models.ForeignKey(Coachee, null=True, blank=True, on_delete=models.CASCADE)
+    session = models.IntegerField()
     jornal = models.TextField(null=True, blank=True)
-    indications1 = models.ForeignKey(Indication, related_name="indication1", null=True, blank=True, on_delete=models.SET_NULL)
-    indications2 = models.ForeignKey(Indication, related_name="indication2", null=True, blank=True, on_delete=models.SET_NULL)
-    indications3 = models.ForeignKey(Indication, related_name="indication3", null=True, blank=True, on_delete=models.SET_NULL)
+    indications1 = models.ForeignKey(Indication, related_name="indication1", null=True, blank=True, on_delete=models.CASCADE)
+    indications2 = models.ForeignKey(Indication, related_name="indication2", null=True, blank=True, on_delete=models.CASCADE)
+    indications3 = models.ForeignKey(Indication, related_name="indication3", null=True, blank=True, on_delete=models.CASCADE)
     concluded = models.BooleanField()
 
     def __str__(self):
-    	return ('Session with ' + self.coachee.name + ' on ' + str(self.date) + ' at ' + str(self.time))
-
-class Report_Session(models.Model):
-    coach = models.ForeignKey(Coach, null=True, blank=True, on_delete=models.SET_NULL)
-    hours_session = models.IntegerField()
-    appointments = models.IntegerField()
-
-    def __str__(self):
-    	return (self.coach.name + ' spent ' + self.hours_session + ' hours in sessions and has ' + self.appointments + ' appointmemts')
+    	return f'Coach {self.coach.name} session number {self.session} with {self.coachee.name}.'
