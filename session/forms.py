396875892ef8c7896656c89635850f1ba19ac9ba
alychinque <alychinque@gmail.com>
2021-08-12 16:42:51 +0100

    Fixed form and getting coach.

--- session/forms.py
+++ session/forms.py
@@ -1,27 +1,29 @@
 from django import forms
+from django.forms import widgets
 from django.forms.widgets import DateInput, TimeInput
-from .models import AppointmentSession
+from .models import AppointmentSession, Coachee
+from datetime import date
 
 
-class AppointmentSessionForm(forms.ModelForm):
+class SessionForm(forms.ModelForm):
+    
+    
     class Meta:
         model = AppointmentSession
-        fields = ('coachee', 'session', 'date', 'time')
+        fields = ('coachee', 'date', 'time',)
+        today = date.today()
+
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date', 'min':today}),
+            'time': forms.TimeInput(attrs={'type': 'time'}),
+        }
 
     def __init__(self, *args, **kwargs):
         """
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
         """
+        coachees = kwargs['coachees']
+        kwargs.pop('coachees')
         super().__init__(*args, **kwargs)
-        placeholders = {
-            'coachee_name': 'Coachee Name',
-            'session': 'Number of Session',
-            'date': 'Date of the session',
-        }
-        widgets = {
-            'date': DateInput(format='%d-%m-%Y'),
-            'time': TimeInput(),
-        }
-
-        self.fields['coachee'].widget.attrs['autofocus'] = True
+        self.fields['coachee'].queryset=coachees   
