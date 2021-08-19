05c7830b9b319bfadb8175e6d0b2948866ca8674
alychinque <alychinque@gmail.com>
2021-08-19 15:45:18 +0100

    Create edit and delete functions for the sessions.

--- session/urls.py
+++ session/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('date/', views.session_date, name='session'),
     path('<int:coach>/', views.coach, name='coach'),
     path('agenda/', views.agenda, name='agenda-coach'),
-    path('agenda-show/<int:id>/', views.agenda_show, name='agenda-show'),
-    path('agenda-edit/<int:id>/', views.agenda_edit, name='agenda-edit'),
+    path('agenda_show/<int:id>/', views.agenda_show, name='agenda-show'),
+    path('date_edit/<int:id>/', views.session_edit, name='date-edit'),
+    path('date_delete/<int:id>/', views.session_delete, name='date-delete'),
 ]
--- session/views.py
+++ session/views.py
@@ -73,7 +73,7 @@ def coach(request, coach):
 
 def agenda(request):
     coach = request.user.coach
-    agenda = AppointmentSession.objects.filter(coach=coach).order_by('time')
+    agenda = AppointmentSession.objects.filter(coach=coach).order_by('date', 'time')
     return render(request, 'session/agenda.html', context= { 'agenda': agenda })
 
 
@@ -85,3 +85,68 @@ def agenda_show(request, id):
 def agenda_edit(request, id):
     
     return render(request, 'session/agenda_edit.html', context={})
+
+
+def session_edit(request, id):
+    """
+    Edits the session 
+    """
+    coach = request.user.coach
+    coachees = Coachee.objects.filter(coach=coach)
+    agenda = AppointmentSession.objects.filter(id=id)
+    for a in agenda:
+        agenda2 = {
+            'coachee': (a.coachee.name),
+            'number_session': (a.number_session),
+            'date': (a.date),
+            'time': (a.time),
+        }
+    
+    form = SessionForm(coachees = coachees)
+    template_name = 'session/date_edit.html'
+    context = {
+        'form': form,
+        'id': coach.id,
+        'agenda': agenda
+    }
+
+    if request.method == "POST":
+        form = SessionForm(request.POST, coachees=coachees)
+        agenda.delete()
+        if form.is_valid():
+            coachee = request.POST.get('coachee')
+            date = request.POST.get('date')
+            sessionNumber = agenda2['number_session']
+            appointmentSession = AppointmentSession()
+            appointmentSession.coach = coach
+            appointmentSession.coachee = Coachee.objects.filter(id=coachee).first()
+            appointmentSession.number_session = sessionNumber
+            appointmentSession.date = date
+            appointmentSession.time = request.POST.get('time')
+            appointmentSession.save()
+
+            template_name = 'pages/coach_page.html'
+            context = {
+                'coachee': coachee,
+                'date': date,
+                'msg': 'Appointment booked'
+            }
+        else:
+            context = {
+            'form': form,
+            'id': coach.id
+            }
+    return render(request, template_name, context)
+
+def session_delete(request, id):
+    """
+    Deletes the session
+    """
+    agenda_delete = AppointmentSession.objects.filter(id=id)
+    agenda_delete.delete()
+    template_name = 'session/agenda.html'
+    coach = request.user.coach
+    agenda = AppointmentSession.objects.filter(coach=coach).order_by('date', 'time')
+
+    return render(request, template_name, context= { 'agenda': agenda })
+    
