82edabb43c4b4aecaaad5c3e39d2b3ac2e598a44
alychinque <alychinque@gmail.com>
2021-09-08 15:54:46 +0100

    Added url for coach_create

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from . import views
 from .views import *
 
 
 app_name = 'profiles'
 
 urlpatterns = [
-    path('coach_create/', CoachCreate.as_view(), name='coach_create'),
+    path('coach_create/', views.coach_create, name='coach_create'),
     path('coach_list/', CoachList.as_view(), name='coach_list'),
     path('coachee_create/', CoacheeCreate.as_view(), name='coachee_create'),
     path('coachee_list/', CoacheeList.as_view(), name='coachee_list'),
