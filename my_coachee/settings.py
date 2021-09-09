e9f1e4ca5ec21ea545f407261fea722a9abd0933
alychinque <alychinque@gmail.com>
2021-09-09 04:23:28 +0100

    Created secret variables.

--- my_coachee/settings.py
+++ my_coachee/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+if os.path.exists("env.py"):
+    import env 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qqk9au6gyf6o)(k!d_#z^yq1ew52!%4ic63bb96=d6vm$%*pk4'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG','')
+
 
 ALLOWED_HOSTS = []
 
@@ -182,6 +186,7 @@ BOOTSTRAP4 = {
     'include_jquery': True,
 }
 
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY','')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY','')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET','')
