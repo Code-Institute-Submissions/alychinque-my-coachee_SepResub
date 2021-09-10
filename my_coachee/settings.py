9358e68f98f17bd5e76f1f847cdc113481c28a1f
alychinque <alychinque@gmail.com>
2021-09-10 13:55:28 +0100

    deployment

--- my_coachee/settings.py
+++ my_coachee/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env 
@@ -30,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','')
 DEBUG = os.environ.get('DEBUG','')
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-coachee,herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +120,18 @@ WSGI_APPLICATION = 'my_coachee.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
