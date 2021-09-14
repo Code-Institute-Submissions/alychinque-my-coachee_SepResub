91e62af80eaabcab835c06536ece52d379445541
alychinque <alychinque@gmail.com>
2021-09-14 06:56:33 +0100

    Created urls for indications edit and delete.

--- indications/urls.py
+++ indications/urls.py
@@ -7,6 +7,6 @@ app_name = 'indications'
 urlpatterns = [
     path('create/', IndicationCreate.as_view(), name='create'),
     path('list/',IndicationList.as_view(), name='list'),
-    # path('indicatione_edit/<int:pk>/', IndicationEdit.as_view(), name='indication_edit'),
-    # path('indication_delete/<int:pk>/', IndicationDelete.as_view(), name='indication_delete'),
+    path('indication_edit/<int:pk>/', IndicationEdit.as_view(), name='indication_edit'),
+    path('indication_delete/<int:pk>/', IndicationDelete.as_view(), name='indication_delete'),
 ]
