aadf0769d6e4b3c8d02f29e69126586fcae24ddc
alychinque <alychinque@gmail.com>
2021-09-23 15:16:59 +0100

    Changed DEVELOPMENT for DEBUG

--- my_coachee/settings.py
+++ my_coachee/settings.py
@@ -215,7 +215,7 @@ STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY','')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY','')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET','')
 
-if 'DEVELOPMENT' in os.environ:
+if 'DEBUG' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'mycoachee@example.com'
 else:
