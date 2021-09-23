430ec2d042aa38900235cb78acb1b9f5b537b15f
alychinque <alychinque@gmail.com>
2021-09-23 11:56:59 +0100

    Renamed urls

--- session/urls.py
+++ session/urls.py
@@ -5,10 +5,10 @@ import session
 app_name = 'session'
 
 urlpatterns = [
-    path('date/', views.session_date, name='session'),
+    path('date/', views.session_date, name='session_date'),
     path('<int:coach>/', views.coach, name='coach'),
-    path('agenda/', views.agenda, name='agenda-coach'),
-    path('agenda_show/<int:id>/', views.agenda_show, name='agenda-show'),
-    path('date_edit/<int:id>/', views.session_edit, name='date-edit'),
-    path('date_delete/<int:id>/', views.session_delete, name='date-delete'),
+    path('agenda/', views.agenda, name='agenda_coach'),
+    path('agenda_show/<int:id>/', views.agenda_show, name='agenda_show'),
+    path('date_edit/<int:id>/', views.session_edit, name='date_edit'),
+    path('date_delete/<int:id>/', views.session_delete, name='date_delete'),
 ]
